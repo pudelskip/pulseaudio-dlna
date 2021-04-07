a4da4129d8b03f373f73e0e4d42c1c52b19e3eee
Paweł Pudelski <pudelskip@gmail.com>
2021-04-07 17:04:31 +0200

    Add setting outputs for applications

--- pulseaudio_dlna/pulseaudio.py
+++ pulseaudio_dlna/pulseaudio.py
@@ -834,6 +834,21 @@ class PulseWatcher(PulseAudio):
                         self.switch_back(bridge, message)
         return False
 
+    def set_device_for_inputs(self, device_name):
+        process = subprocess.Popen(
+            ['pacmd' 'list-sink-inputs'],
+            stdout=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        if process.returncode == 0:
+            apps = re.findall(r'\s+index:\s+(\w+).*\n', stdout)
+            devices = re.findall(r'\s+sink:.+<(.+)>.*\n', stdout)
+            for i in range(len(apps)):
+                if devices[i] != device_name:
+                    ['pactl', 'move-sink-input', str(apps[i]), device_name]
+                    process = subprocess.Popen(['pacmd' 'list-sink-inputs'],stdout=subprocess.PIPE)
+                    _, _ = process.communicate()
+
+
     def add_device(self, device):
         sink = self.create_null_sink(
             device.short_name, device.label)
@@ -850,6 +865,7 @@ class PulseWatcher(PulseAudio):
                 stdout, stderr = process.communicate()
                 if process.returncode == 0:
                     logger.info('Set default sink to {name}'.format(name=device.name))
+                    set_device_for_inputs(str(device.short_name))
                 else:
                     logger.info('Could not default sink to {name}'.format(name=device.name))
                 
